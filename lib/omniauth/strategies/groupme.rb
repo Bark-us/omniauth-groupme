b15de34c60c1391df236eb4765888d4668d0c07d
Brandon Hilkert <brandonhilkert@gmail.com>
2022-09-08 15:52:33 -0700

    Fix strategy for documented Gropume OAuth behavior
    https://dev.groupme.com/tutorials/oauth

--- lib/omniauth/strategies/groupme.rb
+++ lib/omniauth/strategies/groupme.rb
@@ -5,7 +5,7 @@ module OmniAuth
     class Groupme < OmniAuth::Strategies::OAuth2
       option :name, "groupme"
       option :client_options, { site: "https://oauth.groupme.com" }
-      option :token_params, { parse: :json }
+      option :provider_ignores_state, true
       option :auth_token_params, { mode: :query, param_name: :token }
 
       uid { raw_info['id'] }
@@ -26,8 +26,35 @@ module OmniAuth
         }
       end
 
+      def request_phase
+        redirect client.implicit.authorize_url({ redirect_uri: callback_url })
+      end
+
       def raw_info
         @raw_info ||= access_token.get('https://api.groupme.com/v3/users/me').parsed["response"]
+      end
+
+      def callback_phase
+        error = request.params["error_reason"] || request.params["error"]
+        if error
+          fail!(error, CallbackError.new(request.params["error"], request.params["error_description"] || request.params["error_reason"], request.params["error_uri"]))
+        elsif !options.provider_ignores_state && (request.params["state"].to_s.empty? || request.params["state"] != session.delete("omniauth.state"))
+          fail!(:csrf_detected, CallbackError.new(:csrf_detected, "CSRF detected"))
+        else
+          self.access_token = build_access_token(request.params["access_token"])
+          env['omniauth.auth'] = auth_hash
+          call_app!
+        end
+      rescue ::OAuth2::Error, CallbackError => e
+        fail!(:invalid_credentials, e)
+      rescue ::Timeout::Error, ::Errno::ETIMEDOUT => e
+        fail!(:timeout, e)
+      rescue ::SocketError => e
+        fail!(:failed_to_connect, e)
+      end
+
+      def build_access_token(access_token)
+        ::OAuth2::AccessToken.new(client, access_token, options.auth_token_params)
       end
     end
   end
